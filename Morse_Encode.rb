7e10e1e776b815fd0428a82540bf76640c4919d8
Abbas Sarwar <abbassarwar100@gmail.com>
2023-06-14 20:29:30 +0500

    Char & word

---
+++ Morse_Encode.rb
@@ -0,0 +1,47 @@
+CODES_CHAR = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
+
+def decode_char(val)
+  CODES_CHAR[val]
+end
+
+char = decode_char('.-')
+puts char
+
+def decode_word(word)
+  splitted = word.split
+  words = ''
+  splitted.each do |c|
+    words += decode_char(c)
+  end
+  words
+end
+
+puts decode_word('-- -.--')
+
